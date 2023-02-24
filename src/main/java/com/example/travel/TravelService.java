6d081ccf09af4a3c13d1b57fd7e89cd11d31827d
Ismoil <ismoilzaripov55@gmail.com>
2023-02-24 17:51:39 +0500

    A list of the top five data is passed from the repository with the findByTopFiveTravel method, and the result is passed as a list with the mapToResponse method.

--- src/main/java/com/example/travel/TravelService.java
+++ src/main/java/com/example/travel/TravelService.java
@@ -3,8 +3,27 @@ package com.example.travel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class TravelService {
+    private final TravelRepository travelRepository;
 
+    public List<TravelResponse> findByTopFiveTravel() {
+        return travelRepository.findByTopFiveTravel()
+                .stream()
+                .map(this::mapToResponse)
+                .toList();
+    }
+
+    public TravelResponse mapToResponse(TravelEntity e){
+        return TravelResponse.builder()
+                .id(e.getId())
+                .title(e.getTitle())
+                .description(e.getDescription())
+                .photoUrl(e.getPhotoUrl())
+                .createdDate(e.getCreatedDate())
+                .build();
+    }
 }
