e27433a4df46eb5278895b04d11871fb1dd54d2e
Ismoil <ismoilzaripov55@gmail.com>
2023-02-24 20:00:35 +0500

    Added @NotBlank annotation for query validation

--- src/main/java/com/example/travel/TravelRequest.java
+++ src/main/java/com/example/travel/TravelRequest.java
@@ -1,10 +1,14 @@
 package com.example.travel;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class TravelRequest {
+    @NotBlank(message = "Title is empty")
     private String title;
+    @NotBlank(message = "Description is empty")
     private String description;
+    @NotBlank(message = "Photo url is empty")
     private String photoUrl;
 }
