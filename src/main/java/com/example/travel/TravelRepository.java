c6bfc501665cfe01d5a67b8d828c1a09d4b53822
Ismoil <ismoilzaripov55@gmail.com>
2023-02-24 22:52:24 +0500

    getAll method added

--- src/main/java/com/example/travel/TravelRepository.java
+++ src/main/java/com/example/travel/TravelRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
@@ -13,4 +14,7 @@ public interface TravelRepository extends JpaRepository<TravelEntity, Integer> {
 
     @Query("FROM TravelEntity AS t ORDER BY t.createdDate DESC LIMIT 5")
     List<TravelEntity> findByTopFiveTravel();
+
+    @Query("FROM TravelEntity AS t ORDER BY t.createdDate DESC")
+    List<TravelEntity> getAll();
 }
