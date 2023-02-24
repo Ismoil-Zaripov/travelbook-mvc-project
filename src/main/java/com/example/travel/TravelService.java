4b46f1d1bf6702528643ec5787d0a84b9955d1d9
Ismoil <ismoilzaripov55@gmail.com>
2023-02-24 17:13:08 +0500

    Travel Servce added

---
+++ src/main/java/com/example/travel/TravelService.java
@@ -0,0 +1,10 @@
+package com.example.travel;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TravelService {
+
+}
