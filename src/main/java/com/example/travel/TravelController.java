a48f395f3e96a9d55e5acd2bcb26b0ef11339b76
Ismoil <ismoilzaripov55@gmail.com>
2023-02-24 20:07:40 +0500

    Added addTravelPage and addTravel methods to TravelController

--- src/main/java/com/example/travel/TravelController.java
+++ src/main/java/com/example/travel/TravelController.java
@@ -1,9 +1,13 @@
 package com.example.travel;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,6 +23,23 @@ public class TravelController {
 
         List<TravelResponse> travels = travelService.findByTopFiveTravel();
         model.addAttribute("travels", travels);
+        return "index";
+    }
+
+    @GetMapping("/add-page")
+    public String addTravelPage(Model model){
+        model.addAttribute("travel", new TravelRequest());
+        return "travel-add";
+    }
+
+    @PostMapping("/add")
+    public String addTravel(
+            @Valid
+            @ModelAttribute("travel") TravelRequest request,
+            BindingResult result
+    ){
+        if (result.hasErrors()) return "travel-add";
+        travelService.save(request);
         return "index";
     }
 }
