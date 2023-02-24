c703f7d2055bba48f027b97f8b83f96eecf109f2
Ismoil <ismoilzaripov55@gmail.com>
2023-02-24 19:02:23 +0500

    Added TravelRequest object to receive requests

---
+++ src/main/java/com/example/travel/TravelRequest.java
@@ -0,0 +1,10 @@
+package com.example.travel;
+
+import lombok.Data;
+
+@Data
+public class TravelRequest {
+    private String title;
+    private String description;
+    private String photoUrl;
+}
